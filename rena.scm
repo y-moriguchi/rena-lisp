8037c49ad69b6bc16f776a187cd74d0d9ca59b99
ymoriguchi <yuichiro-moriguchi@nifty.com>
2019-04-14 10:35:41 +0900

    add delimit and lookahead

--- rena.scm
+++ rena.scm
@@ -13,6 +13,10 @@
         (call-with-values
             (lambda () ((car opt) str index0 #f))
           (lambda (match lastindex syn) (if match lastindex index0)))))
+  (define (substring-over string beginindex endindex)
+    (if (< beginindex endindex)
+        (substring string beginindex endindex)
+        ""))
   (define (renathen . args)
     (lambda (str lastindex0 attr)
       (let loop
@@ -44,7 +48,7 @@
                   (if match
                       (values match lastindex syn)
                       (loop (cdr args))))))))))
-  (define (times ptn mincount maxcount . opt)
+  (define (times mincount maxcount ptn . opt)
     (lambda (str lastindex0 attr)
       (let ((ptn (wrap ptn))
             (action (if (and (pair? opt) (car opt))
@@ -64,11 +68,54 @@
                   (if (or (< count mincount)
                           (and maxcount (> count maxcount)))
                       (values #f #f #f)
-                      (if (= lastindex0 indexloop)
-                          (values "" indexloop attr)
-                          (values (substring str lastindex0 indexloop)
-                                  indexloop
-                                  attr))))))))))
+                      (values (substring-over str lastindex0 indexloop)
+                              indexloop
+                              attr)))))))))
+  (define (delimit ptn delimiter . opt)
+    (lambda (str lastindex0 attr)
+      (let ((ptn (wrap ptn))
+            (delimiter (wrap delimiter))
+            (action (if (and (pair? opt) (car opt))
+                        (car opt)
+                        (lambda (match syn inh) syn))))
+        (let loop ((lastindex lastindex0)
+                   (index-before lastindex0)
+                   (attr attr)
+                   (matched-once #f))
+          (call-with-values
+              (lambda () (ptn str lastindex attr))
+            (lambda (match indexnew attrnew)
+              (if match
+                  (call-with-values
+                      (lambda ()
+                        (delimiter str
+                                   (indexignore str indexnew)
+                                   #f))
+                    (lambda (dmatch dindexnew dattr)
+                      (if dmatch
+                          (loop (indexignore str dindexnew)
+                                indexnew
+                                (action match attrnew attr)
+                                #t)
+                          (values (substring str lastindex0 indexnew)
+                                  indexnew
+                                  (action match attrnew attr)))))
+                  (if matched-once
+                      (values (substring-over str lastindex0 index-before)
+                              index-before
+                              attr)
+                      (values #f #f #f)))))))))
+  (define (lookahead ptn pos)
+    (lambda (string lastindex attr)
+      (let ((ptn (wrap ptn)))
+            (call-with-values
+                (lambda () (ptn string lastindex attr))
+              (lambda (match indexnew attrnew)
+                (if (or (and match pos) (and (not match) (not pos)))
+                    (values "" lastindex attr)
+                    (values #f #f #f)))))))
+  (define (set-attr attrnew)
+    (lambda (string lastindex attr) (values "" lastindex attrnew)))
   (define (renastr str)
     (lambda (mstr lastindex attr)
       (if (< (+ lastindex (string-length str) -1) (string-length mstr))
@@ -150,38 +197,47 @@
       (delay
         (let ((r (force rr)))
           (r 'one-or-more (r 'or " " "\t" "\n")))))
+    (define renabr
+      (delay
+        (let ((r (force rr)))
+          (r 'or "\r\n" "\r" "\n"))))
     (let ((farg (car args)))
       (cond ((eq? farg 'then) (apply renathen (cdr args)))
             ((eq? farg 'or) (apply renaor (cdr args)))
             ((eq? farg 'times) (apply times (cdr args)))
             ((eq? farg 'at-least)
              (times (cadr args)
-                    (caddr args)
                     #f
+                    (caddr args)
                     (and (pair? (cdddr args)) (cadddr args))))
             ((eq? farg 'at-most)
-             (times (cadr args)
-                    0
+             (times 0
+                    (cadr args)
                     (caddr args)
                     (and (pair? (cdddr args)) (cadddr args))))
             ((eq? farg 'one-or-more)
-             (times (cadr args)
-                    1
+             (times 1
                     #f
+                    (cadr args)
                     (and (pair? (cddr args)) (caddr args))))
             ((eq? farg 'zero-or-more)
-             (times (cadr args)
-                    0
+             (times 0
                     #f
+                    (cadr args)
                     (and (pair? (cddr args)) (caddr args))))
-            ((eq? farg 'maybe) (r 'times (cadr args) 0 1))
+            ((eq? farg 'maybe) (r 'times 0 1 (cadr args)))
+            ((eq? farg 'delimit) (apply delimit (cdr args)))
+            ((eq? farg 'lookahead) (lookahead (cadr args) #t))
+            ((eq? farg 'lookahead-not) (lookahead (cadr args) #f))
             ((eq? farg 'range) (renarange (cadr args) (caddr args)))
             ((eq? farg 'complement) (apply renarange-cmp (cdr args)))
+            ((eq? farg 'attr) (set-attr (cadr args)))
             ((eq? farg 'any) rena-any)
             ((eq? farg 'real)
              (r (force renafloat)
                 (lambda (match syn inh) (string->number match))))
             ((eq? farg 'whitespace) (force renaspc))
+            ((eq? farg 'br) (force renabr))
             ((eq? farg 'y) (apply y-letrec (cdr args)))
             ((null? (cdr args)) (wrap farg))
             (else (wrapaction (wrap farg) (cadr args))))))
